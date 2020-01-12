0645dcb017e679169657fa2ff87cff7145671cfc
Quin <cheesits456@haileybot.com>
2020-01-12 02:59:02 -0800

    Update index.js

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ client.config = JSON.parse(fs.readFileSync("config.json", "utf8"));
 
 client.on("message", msg => {
 	if (msg.channel.id === client.config.channel && msg.author.id === client.config.owner) exec(msg.content, (err, stdout, stderr) => {
-		if (err) return console.error(err);
+		if (err) console.error(err);
 		if (stdout) msg.channel.send("```" + stdout + "```");
 		if (stderr) msg.channel.send("```" + stderr + "```");
 	});
 });
+
+client.on("ready", () => console.log(`Logged in as ${client.user.tag}`));
 
 client.login(client.config.token);
