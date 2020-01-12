c6308529f7f01879bc276c2e8d9dff081e7df09b
Quin <cheesits456@haileybot.com>
2020-01-12 02:53:04 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const { exec } = require('child_process');
+const fs = require("fs");
+const Discord = require("discord.js");
+
+let client = new Discord.Client();
+client.config = JSON.parse(fs.readFileSync("config.json", "utf8"));
+
+client.on("message", msg => {
+	if (msg.channel.id === client.config.channel && msg.author.id === client.config.user) exec(msg.content, (err, stdout, stderr) => {
+		if (err) return console.error(err);
+		if (stdout) msg.channel.send("```" + stdout + "```");
+		if (stderr) msg.channel.send("```" + stderr + "```");
+	});
+});
+
+client.login(client.config.token);
