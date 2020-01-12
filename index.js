4b48e05e270ddb5100f40a97b50ac4dd1303c203
Quin <cheesits456@haileybot.com>
2020-01-12 04:00:09 -0800

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,19 @@ const Discord = require("discord.js");
 
 let client = new Discord.Client();
 client.config = JSON.parse(fs.readFileSync("config.json", "utf8"));
+const options = {
+	split: {
+		char: "\n",
+		prepend: "```",
+		append: "```"
+	}
+}
 
 client.on("message", msg => {
 	if (msg.channel.id === client.config.channel && msg.author.id === client.config.owner) exec(msg.content, (err, stdout, stderr) => {
 		if (err) console.error(err);
-		if (stdout) msg.channel.send("```" + stdout + "```");
-		if (stderr) msg.channel.send("```" + stderr + "```");
+		if (stdout) msg.channel.send("```" + Discord.escapeMarkdown(stdout, true) + "```", options);
+		if (stderr) msg.channel.send("```" + Discord.escapeMarkdown(stderr, true) + "```", options);
 	});
 });
 
