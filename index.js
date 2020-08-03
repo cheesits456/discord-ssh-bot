68d9d419366d732a04a3bcec85aa13cf6decc096
Quin <quin@cheesits456.dev>
2020-08-03 04:39:59 -0700

    tidied up the code and updated it for d.js v12

--- index.js
+++ index.js
@@ -5,18 +5,14 @@ const Discord = require("discord.js");
 let client = new Discord.Client();
 client.config = JSON.parse(fs.readFileSync("config.json", "utf8"));
 const options = {
-	split: {
-		char: "\n",
-		prepend: "```",
-		append: "```"
-	}
+	code: "js",
+	split: { char: "\n" }
 }
 
 client.on("message", msg => {
 	if (msg.channel.id === client.config.channel && msg.author.id === client.config.owner) exec(msg.content, (err, stdout, stderr) => {
-		if (err) console.error(err);
-		if (stdout) msg.channel.send("```\n" + Discord.escapeMarkdown(stdout, true) + "```", options);
-		if (stderr) msg.channel.send("```\n" + Discord.escapeMarkdown(stderr, true) + "```", options);
+		if (stdout) msg.channel.send(Discord.Util.cleanCodeBlockContent(stdout, true), options);
+		if (stderr) msg.channel.send(Discord.Util.cleanCodeBlockContent(stderr, true), options);
 	});
 });
 
